0d61f50fd5d2ee8986efdd638a581f953f1b9146
JiyoungHyun <shyunji023@gmail.com>
2022-05-24 21:17:18 +0900

    feat: poSearch api 연결

--- src/apis/po.api.js
+++ src/apis/po.api.js
@@ -5,8 +5,6 @@ const PREFIX_URL = "/po";
 export const getPoList = async () => {
   try {
     const { data } = await serverAxios.get(`${PREFIX_URL}/po1List`);
-
-    console.log(data);
     // const { lectures, category, skill } = data.data;
 
     // const result = lectures.map((response) => {
@@ -34,32 +32,19 @@ export const getPoList = async () => {
   }
 };
 
+export const getSearchPoList = async (test) => {
+  try {
+    // TODO: GET 시도해보기
+    const { data } = await serverAxios.post(`${PREFIX_URL}/poSearch`,test);
+    return data;
+  } catch (err) {
+    throw new Error("Failed to load custom process result");
+  }
+};
+
 export const getPoLov = async () => {
   try {
     const { data } = await serverAxios.get(`${PREFIX_URL}/poCdv`);
-
-    console.log(data);
-    // const { lectures, category, skill } = data.data;
-
-    // const result = lectures.map((response) => {
-    //   return {
-    //     name: response.name,
-    //     time: response.time,
-    //     price: response.price,
-    //     replay: response.duration,
-    //     answer: response.reviewTime,
-    //     createdDate: response.startYear,
-    //     tags: response.tags,
-    //     url: response.url,
-    //     site: response.site,
-    //   };
-    // });
-
-    // return {
-    //   result,
-    //   category: { id: category.id, name: category.name },
-    //   skill: { id: skill.id, name: skill.name },
-    // };
     return data;
   } catch (err) {
     throw new Error("Failed to load custom process result");
--- src/pages/SelectPoList.jsx
+++ src/pages/SelectPoList.jsx
@@ -1,10 +1,20 @@
-import { getPoList, getPoLov } from "apis/po.api";
+import { getPoList, getPoLov, getSearchPoList } from "apis/po.api";
 import Input from "components/common/Input";
 import InputSearch from "components/common/InputSearch";
 import InputSelect from "components/common/InputSelect";
 import React, { useEffect, useState } from "react";
-
+import DataGrid from "react-data-grid";
 function SelectPoList() {
+  const columns = [
+    { key: "id", name: "ID" },
+    { key: "title", name: "Title" },
+  ];
+
+  const initialRows = [
+    { id: 0, title: "Example" },
+    { id: 1, title: "Demo" },
+  ];
+  const [rows, setRows] = useState(initialRows);
   const [poCondition, setPoCondition] = useState({
     contractName: "",
     suplier: "",
@@ -22,95 +32,97 @@ function SelectPoList() {
   const [poType, setPoType] = useState([]);
 
   const handlePoCondition = (key, value) => {
-    console.log(key);
     const tempPoCondition = { ...poCondition };
 
     tempPoCondition[key] = value;
     setPoCondition(tempPoCondition);
   };
   const selectPoList = async () => {
-    const poListData = await getPoList();
-
-    console.log(poListData);
     console.log(poCondition);
+
+    const poListData = await getSearchPoList(poCondition);
+    console.log(poListData);
   };
   const getLov = async () => {
     const data = await getPoLov();
-
     data && setPoGroupLov(data);
   };
 
   useEffect(() => {
     getLov();
-    console.log(poGroupLov);
   }, []);
 
   return (
-    <section>
-      <div>
-        <button onClick={selectPoList}>조회</button>
-      </div>
-      <div>
-        <Input
-          id="contractName"
-          inputLabel="계약명"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.contractName}
-        />
-        <InputSearch
-          id="suplier"
-          inputLabel="공급사"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.suplier}
-        />
-        <InputSelect
-          id="poGroup"
-          inputLabel="계약구분"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.poGroup}
-          lov={poGroupLov}
-        />
-        {/* <InputSelect id="poApprove" inputLabel="PO 승인" />*/}
-        <Input
-          id="poNum"
-          inputLabel="PO 번호"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.poNum}
-        />
-        <InputSearch
-          id="item"
-          inputLabel="Item"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.item}
-        />
-        <Input
-          id="prNum"
-          inputLabel="PR 번호"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.prNum}
-        />
-        <Input
-          id="rfqNum"
-          inputLabel="RFQ번호"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.rfqNum}
-        />
-        {/* <InputSelect id="saso" inputLabel="사소" /> */}
-        <InputSearch
-          id="prRequestor"
-          inputLabel="PR 신청자"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.prRequestor}
-        />
-        <InputSearch
-          id="buyer"
-          inputLabel="Buyer"
-          handlePoCondition={handlePoCondition}
-          inputValue={poCondition.buyer}
-        />
-        {/* <InputSelect id="poType" inputLabel="Type" /> */}
-      </div>
-    </section>
+    <>
+      <section>
+        <div>
+          <button onClick={selectPoList}>조회</button>
+        </div>
+        <div>
+          <Input
+            id="contractName"
+            inputLabel="계약명"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.contractName}
+          />
+          <InputSearch
+            id="suplier"
+            inputLabel="공급사"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.suplier}
+          />
+          <InputSelect
+            id="poGroup"
+            inputLabel="계약구분"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.poGroup}
+            lov={poGroupLov}
+          />
+          {/* <InputSelect id="poApprove" inputLabel="PO 승인" />*/}
+          <Input
+            id="poNum"
+            inputLabel="PO 번호"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.poNum}
+          />
+          <InputSearch
+            id="item"
+            inputLabel="Item"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.item}
+          />
+          <Input
+            id="prNum"
+            inputLabel="PR 번호"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.prNum}
+          />
+          <Input
+            id="rfqNum"
+            inputLabel="RFQ번호"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.rfqNum}
+          />
+          {/* <InputSelect id="saso" inputLabel="사소" /> */}
+          <InputSearch
+            id="prRequestor"
+            inputLabel="PR 신청자"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.prRequestor}
+          />
+          <InputSearch
+            id="buyer"
+            inputLabel="Buyer"
+            handlePoCondition={handlePoCondition}
+            inputValue={poCondition.buyer}
+          />
+          {/* <InputSelect id="poType" inputLabel="Type" /> */}
+        </div>
+      </section>
+      <section>
+        <DataGrid columns={columns} rows={rows} onRowsChange={setRows} />
+      </section>
+    </>
   );
 }
 
