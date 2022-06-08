a41eabdad48e1294993be8d96f1d0d58cea7ec9b
jiyoung <shyunji023@gmail.com>
2022-06-08 17:16:49 +0900

    fix: REPLY_METHOD_LOOKUP_CODE

--- src/stores/colData.js
+++ src/stores/colData.js
@@ -5,7 +5,7 @@ export const bidColFields =[
   { colId:4, field:"rfq_DESCRIPTION", headerName: "건명", minWidth:300},
   { colId:5, field:"simple_QUOTATION_FLAG", headerName: "단계", minWidth:100},
   { colId:6, field:"bid_SEARCH_TYPE", headerName: "Status", minWidth:100},
-  { colId:7, field:"bid_TYPE_CODE", headerName: "입찰방법", minWidth:150},
+  { colId:7, field:"reply_METHOD_LOOKUP_CODE", headerName: "입찰방법", minWidth:150},
   { colId:8, field:"bid_PRICE_METHOD", headerName: "단가입력방법", minWidth:150},
   { colId:9, field:"bidding_START_DATE", headerName: "입찰개시일시", minWidth:200},
   { colId:10, field:"bidding_END_DATE", headerName: "입찰마감일시", minWidth:200},
