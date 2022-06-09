dc0d18f11fe4e9d2d24ad1ea61dc3accf6010bae
JiyoungHyun <shyunji023@gmail.com>
2022-06-09 21:49:26 +0900

    feat: colData 소문자 수정

--- src/stores/colData.js
+++ src/stores/colData.js
@@ -1,15 +1,15 @@
 export const bidColFields =[
-  { colId:1, field:"category_SEGMENT", headerName: "부문", minWidth:100, },
-  { colId:2, field:"rfq_NO", headerName: "RFQ번호", minWidth:150 },
-  { colId:3, field:"bidding_NO", headerName: "입찰번호", minWidth:150},
-  { colId:4, field:"rfq_DESCRIPTION", headerName: "건명", minWidth:300},
-  { colId:5, field:"simple_QUOTATION_FLAG", headerName: "단계", minWidth:100},
-  { colId:6, field:"bid_SEARCH_TYPE", headerName: "Status", minWidth:100},
-  { colId:7, field:"reply_METHOD_LOOKUP_CODE", headerName: "입찰방법", minWidth:150},
-  { colId:8, field:"bid_PRICE_METHOD", headerName: "단가입력방법", minWidth:150},
-  { colId:9, field:"bidding_START_DATE", headerName: "입찰개시일시", minWidth:200},
-  { colId:10, field:"bidding_END_DATE", headerName: "입찰마감일시", minWidth:200},
-  { colId:11, field:"buyer_ID", headerName: "Buyer", minWidth:100},
+  { colId:1, field:"category_segment", headerName: "부문", minWidth:100, },
+  { colId:2, field:"rfq_no", headerName: "RFQ번호", minWidth:150 },
+  { colId:3, field:"bidding_no", headerName: "입찰번호", minWidth:150},
+  { colId:4, field:"rfq_description", headerName: "건명", minWidth:300},
+  { colId:5, field:"simple_quotation_flag", headerName: "단계", minWidth:100},
+  { colId:6, field:"bid_search_type", headerName: "Status", minWidth:100},
+  { colId:7, field:"reply_method_lookup_code", headerName: "입찰방법", minWidth:150},
+  { colId:8, field:"bid_price_method", headerName: "단가입력방법", minWidth:150},
+  { colId:9, field:"bidding_start_date", headerName: "입찰개시일시", minWidth:200},
+  { colId:10, field:"bidding_end_date", headerName: "입찰마감일시", minWidth:200},
+  { colId:11, field:"buyer_id", headerName: "Buyer", minWidth:100},
   ];
 
 // #region PR
