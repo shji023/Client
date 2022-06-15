0f2dfbd86dfe40998afb994bc4616ec557ab36f6
DESKTOP-UU34RB4\jiyeon <jiueon6@gmail.com>
2022-06-15 11:30:43 +0900

    vendor select

--- src/pages/RfqDetail.jsx
+++ src/pages/RfqDetail.jsx
@@ -1,4 +1,4 @@
-import { getRfqInfo } from "apis/rfq.api";
+import { getRfqInfo, getSearchVendorList } from "apis/rfq.api";
 import { getBidTypeLov, getBidPriceMethodLov, getBidMethodTypeLov, getBidMaxRoundLov, getBidCurrencyCodeLov, insertOneBid} from "apis/bid.api";
 import { colors } from "assets/styles/color";
 import React, { useEffect, useState} from "react";
@@ -13,6 +13,8 @@ import BidInsertTextArea from "components/bid/BidInsertTextArea";
 import InputSelect from "components/common/InputSelect";
 import InputDate from "components/common/InputDate";
 import InputInfo from "components/common/InputInfo";
+import BidInputSelect from "components/bid/BidInputSelect";
+import RfqSelectVendor from "components/rfq/RfqSelectVendor";
 
 function RfqDetail() {
   const {id} = useParams();
@@ -37,25 +39,17 @@ function RfqDetail() {
     "category_id": 0,
     "bidding_fob": "",
   });
-  const [rfqListData, setRfqListData] = useState({});
+  const [rfqListData, setRfqListData] = useState({}); 
   const [ruleInfoData, setRuleInfoData] = useState([]);
-  const [rfqInfoData, setRfqInfoData] = useState([]);
- 
-  // const selectInfo = async () => {
-  //   const ruleInfo = await getRuleInfo(id);
-  //   const rfqInfo = await getRfqInfo(id);
-  //   ruleInfo && setRuleInfoData(ruleInfo[0]);
-  //   rfqInfo && setRfqInfoData(rfqInfo[0]);
-  // };
+
   const roundPeriod = ruleInfoData.round_start_date + ' - ' + ruleInfoData.round_end_date;
   const stage = rfqListData?.simple_quotation_flag === 'Y'? '단순견적':'입찰';
 
   const selectRFQDetail = async (id) => {
     const data = await getRfqInfo(id);
-    // console.log("rfq select data",data);
+    console.log("rfq select data",data);
     setRfqListData(data[0]);
   };
-
 
   // Lov
   const [bidTypeLov, setBidTypeLov] = useState([]);
@@ -90,7 +84,6 @@ function RfqDetail() {
     // !: axios 비동기
     const data = await insertOneBid(bidCondition);
     console.log("완료 : ", data);
-
 
     if(data === 'success'){
       alert("입찰룰이 완료되었습니다.");
@@ -153,9 +146,8 @@ function RfqDetail() {
 
   useEffect(() => {
     selectRFQDetail(id);
-    getLov();
-    
-    // selectInfo();
+    // selectVendorList(id);
+    getLov(); 
   }, []);
 
   console.log("insert - bidCondition : ", bidCondition);
@@ -169,80 +161,81 @@ function RfqDetail() {
       </ButtonWrapper>
       <SubTitle>RFQ 정보</SubTitle>
       <section>
-        <InputContainer>
-        <BidInfo label="RFQ 번호" value={rfqListData.rfq_no}/>
-        <BidInfo label="단계" value={stage}/>
-        <BidInfo label="Status" value={rfqListData.cd_v_meaning_status}/>
-        <BidInfo label="Type" value={rfqListData.cd_v_meaning_type}/>
-        <BidInfo label='건명' value={rfqListData.rfq_description}/> 
-        <BidInfo label='담당자' value={rfqListData.buyer_name +" / "+rfqListData.buyer_dept_name +" / "+rfqListData.buyer_contact}/>
-        <BidInfo label="정산주기" value={rfqListData.po_payment_cycle}/>
-        <BidInfo label="협업 유형" value={rfqListData.po_collabo_type}/>
-        <BidInfo label="계약 기간(BPA)" value={rfqListData.start_date}/>
-        {/* <BidInfo label="계약 기간(BPA)" value={rfqCondition.END_DATE}/> */}
-        <BidInfo label="Amount Limit" value={rfqListData.amount_limit}/>
-        <BidInfo label="납품 지역" value={rfqListData.rfq_ship_to}/>
-        <BidInfo label="지불 조건" value={rfqListData.rfq_payment_terms}/>
-        <BidInfo label="인도 조건" value={rfqListData.bidding_fob}/>
-        </InputContainer>
+        <RfqInfoContainer>
+          <BidInfo label="RFQ 번호" value={id}/>
+          {/* <BidInfo label="RFQ 번호" value={rfqListData.rfq_no}/> */}
+          <BidInfo label="단계" value={stage}/>
+          <BidInfo label="Status" value={rfqListData.cd_v_meaning_status}/>
+          <BidInfo label="Type" value={rfqListData.cd_v_meaning_type}/>
+          <BidInfo label='건명' value={rfqListData.rfq_description}/> 
+          <BidInfo label='담당자' value={rfqListData.buyer_name +" / "+rfqListData.buyer_dept_name +" / "+rfqListData.buyer_contact}/>
+          <BidInfo label="정산주기" value={rfqListData.po_payment_cycle}/>
+          <BidInfo label="협업 유형" value={rfqListData.po_collabo_type}/>
+          <BidInfo label="계약 기간(BPA)" value={rfqListData.start_date}/>
+          {/* <BidInfo label="계약 기간(BPA)" value={rfqCondition.END_DATE}/> */}
+          <BidInfo label="Amount Limit" value={rfqListData.amount_limit}/>
+          <BidInfo label="납품 지역" value={rfqListData.rfq_ship_to}/>
+          <BidInfo label="지불 조건" value={rfqListData.rfq_payment_terms}/>
+          <BidInfo label="인도 조건" value={rfqListData.bidding_fob}/>
+          <BidInfo label="" value={null}/>
+        </RfqInfoContainer>
       </section>
-        <SubTitle>공급사 선정</SubTitle>
-       
-        {/* <RFQAgGridInsertBid listData={rfqListData}/> */}
-        <SubTitle>입찰 룰 (승인상태 : 미승인)</SubTitle>
+      <SubTitle>공급사 선정</SubTitle><br/>
+        <RfqSelectVendor id={id} ></RfqSelectVendor>
+      <SubTitle>입찰 룰 (승인상태 : 미승인)</SubTitle>
         <section>
-        <InfoContainer>
-          <InputSelect
-            id="bid_type_code"
-            inputLabel="입찰유형"
-            handlePoCondition={handleBidCondition}
-            lov={bidTypeLov}
-          />
-          <InputSelect
-            id="bid_price_method"
-            inputLabel="단가입력방식"
-            handlePoCondition={handleBidCondition}
-            lov={bidPriceMethodLov}
-          />
-          <InputSelect
-            id="bid_method_type"
-            inputLabel="낙찰제도"
-            handlePoCondition={handleBidCondition}
-            lov={bidMethodTypeLov}
-          />
-          <InputSelect
-            id="max_round"
-            inputLabel="Max 라운드"
-            handlePoCondition={handleBidCondition}
-            lov={bidMaxRoundLov}
-          />
-          <InputDate
-            id="roundPeriod"
-            inputLabel="라운드 시작/마감"
-            handleCondition={handleBidCondition}
-          />
-          {/* <BidInfo label='라운드 시작/마감' value={roundPeriod}></BidInfo> */}
-           <InputSelect
-            id="main_currency"
-            inputLabel="통화"
-            handlePoCondition={handleBidCondition}
-            lov={bidCurrencyCodeLov}
-          />
-          <InputInfo
-            id="side_conditions"
-            inputLabel="부가조건"
-            handlePoCondition={handleBidCondition}
-            inputValue={bidCondition.side_conditions}
-          />
-          <InputInfo
-            id="target_price"
-            inputLabel="Target Price"
-            handlePoCondition={handleBidCondition}
-            inputValue={bidCondition.target_price}
-          />
-          <BidInsertTextArea id="note_to_bidder" inputLabel='안내사항' handleCondition={handleBidCondition} inputValue={bidCondition.note_to_bidder}></BidInsertTextArea>
-          <BidInsertTextArea id="bidding_rule_approval_comment" inputLabel='내부 보고' handleCondition={handleBidCondition} inputValue={bidCondition.bidding_rule_approval_comment}></BidInsertTextArea>
-          </InfoContainer>
+          <BidInfoContainer>
+            <InputSelect
+              id="bid_type_code"
+              inputLabel="입찰유형"
+              handlePoCondition={handleBidCondition}
+              lov={bidTypeLov}
+            />
+            <InputSelect
+              id="bid_price_method"
+              inputLabel="단가입력방식"
+              handlePoCondition={handleBidCondition}
+              lov={bidPriceMethodLov}
+            />
+            <InputSelect
+              id="bid_method_type"
+              inputLabel="낙찰제도"
+              handlePoCondition={handleBidCondition}
+              lov={bidMethodTypeLov}
+            />
+            <InputSelect
+              id="max_round"
+              inputLabel="Max 라운드"
+              handlePoCondition={handleBidCondition}
+              lov={bidMaxRoundLov}
+            />
+            <InputDate
+              id="roundPeriod"
+              inputLabel="라운드 시작/마감"
+              handleCondition={handleBidCondition}
+            />
+            {/* <BidInfo label='라운드 시작/마감' value={roundPeriod}></BidInfo> */}
+            <InputSelect
+              id="main_currency"
+              inputLabel="통화"
+              handlePoCondition={handleBidCondition}
+              lov={bidCurrencyCodeLov}
+            />
+            <InputInfo
+              id="side_conditions"
+              inputLabel="부가조건"
+              handlePoCondition={handleBidCondition}
+              inputValue={bidCondition.side_conditions}
+            />
+            <InputInfo
+              id="target_price"
+              inputLabel="Target Price"
+              handlePoCondition={handleBidCondition}
+              inputValue={bidCondition.target_price}
+            />
+            <BidInsertTextArea id="note_to_bidder" inputLabel='안내사항' handleCondition={handleBidCondition} inputValue={bidCondition.note_to_bidder}></BidInsertTextArea>
+            <BidInsertTextArea id="bidding_rule_approval_comment" inputLabel='내부 보고' handleCondition={handleBidCondition} inputValue={bidCondition.bidding_rule_approval_comment}></BidInsertTextArea>
+          </BidInfoContainer>
         </section>
 
         {/* RFQ 첨부 */}
@@ -257,6 +250,56 @@ const StyledRoot = styled.main`
   flex-direction: column;
   width: 100%;
   height: 100%;
+`;
+
+const RfqInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, minmax(27rem, 1fr));
+  padding: 2rem 2rem 2rem 0.5rem;
+  & > div:nth-of-type(4) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-of-type(6) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-of-type(10) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-of-type(14) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-child(n+11):nth-child(-n+14){
+    border-bottom: 1px solid ${colors.tableLineGray};
+  }
+`;
+
+const BidInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, minmax(27rem, 1fr));
+  padding: 2rem 2rem 2rem 0.5rem;
+  & > div:nth-of-type(4) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-of-type(7) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
+  & > div:nth-of-type(8) {
+    & > div:nth-of-type(2) {
+      border-right: 1px solid ${colors.tableLineGray};
+    }
+  }
 `;
 
 const InputContainer = styled.div`
@@ -322,8 +365,8 @@ const Title = styled.p`
 
 const SubTitle = styled.p`
   font-size: 1.8rem;
-  margin-bottom: 1rem;
   margin-top: 1rem;
+  margin-left: 1rem;
   width: 90%;
   height: 100%;
 `;
