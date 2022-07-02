c4dd44d6820f1c8402e6f868f27404a7a97cffd6
soswa <sksaaa00@daum.net>
2022-07-02 15:48:50 +0900

    fix: 파일 다운로드 decode 추가

--- src/apis/file.api.js
+++ src/apis/file.api.js
@@ -67,12 +67,14 @@ export const downloadFile = async (path) => {
     .then((response) => {
       console.log("response ::: ", response);
       const name = response.headers['content-disposition'].split('fileName=')[1]
+      const decodedName = decodeURIComponent(name);
       console.log(response.headers)
       console.log(name)
+      console.log(decodeURIComponent(name))
       const url = window.URL.createObjectURL(new Blob([response.data]))
       const link = document.createElement('a')
       link.href = url
-      link.setAttribute('download', name)
+      link.setAttribute('download', decodedName)
       link.style.cssText = 'display:none'
       document.body.appendChild(link)
       link.click()
